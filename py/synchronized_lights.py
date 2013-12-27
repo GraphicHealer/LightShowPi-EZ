one more bug introduced with cleanup - this one around level calculation, really need to get some unit tests to avoid these breaks ;)

py/synchronized_lights.py
@@ -180,7 +180,7 @@ def calculate_levels(data, sample_rate, frequency_limits):
     # Find average 'amplitude' for specific frequency ranges in Hz
     power = np.abs(fourier)
 
-    matrix = []
+    matrix = [0 for i in range(hc.GPIOLEN)]
     for i in range(hc.GPIOLEN):
         matrix[i] = np.mean(power[piff(frequency_limits[i][0], sample_rate)
                                   :piff(frequency_limits[i][1], sample_rate):1])