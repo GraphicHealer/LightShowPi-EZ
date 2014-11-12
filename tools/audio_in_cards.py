885abeff9859320a59d3afeb297b8138f9c7fa51
Todd Giles <todd@lightshowpi.com>
2014-11-11 19:43:23 -0700

    Update some comments on audio-in mode, including links to new wiki page.  Also add tool wrapper around alsaaudio.cards()

---
+++ tools/audio_in_cards.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#
+# Licensed under the BSD license.  See full license in LICENSE file.
+# http://www.lightshowpi.com/
+#
+# Author: Todd Giles (todd@lightshowpi.com)
+"""Trivial wrapper around alsaaduio.cards() for getting a list of your audio cards.
+
+Helpful in determining the list of USB audio cards in advanced audio-in setups.
+https://bitbucket.org/togiles/lightshowpi/wiki/Audio-In%20Mode
+
+Sample usage:
+python audio_in_cards.py
+"""
+
+import alsaaudio as aa
+
+if __name__ == "__main__":
+    aa.cards()
