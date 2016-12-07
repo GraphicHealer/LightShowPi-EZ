40e10919140792b13722302d048ffd4ce85b963d
KenB <broken2048@yahoo.com>
2016-12-07 11:55:11 -0600

    added documentation

--- py/tweet.py
+++ py/tweet.py
@@ -2,7 +2,26 @@
 
 Author : Brett Reinhard
 
-<Instructions for obtaining KEYs and SECRETs>
+To setup automated tweeting, you will need a twitter account with a verified phone number.
+Go to the apps.twitter.com, login to your twitter account. 
+Create a new app with the following information: 
+ Name (of the app)
+ Description
+ A url in http(s)://domain.com 
+Agree to the terms. 
+Click on the Keys and Access Tokens tab
+Consumer Key, and Consumer Secret Key are automatically generated. 
+Under 'Your Access Token', select generate access tokens. 
+This will generate Access Token, and Access Token Secret.
+You now have access to your newly generated Access Token, and Access Token Secret.
+Fill the tokens (KEYS) and secrets in the locations below where 'USER INPUTS DATA' 
+CONSUMER_KEY = 'consumer_token' 
+CONSUMER_SECRET = 'consumer_token_secret' 
+ACCESS_KEY = 'access_token' 
+ACCESS_SECRET = 'access_token_secret'
+
+Ensure that the keys are enclosed by single quotes.
+
 
 """
 
